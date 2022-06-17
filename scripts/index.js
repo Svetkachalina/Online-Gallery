ff8422b01940f7053ea4a93b3fb7e00322be6ace
Svetlana Murdoch <66595456+Svetkachalina@users.noreply.github.com>
2022-06-17 12:44:56 +0300

    Add files via upload

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,23 @@
 import { getData } from "./getData.js";
 import { renderGallery } from "./renderGallery.js";
 
-const init = async () => {
-    const photos = await getData();
-    renderGallery(photos);
+const init = async ({selectorGalleryWrapper, selectorPhotoWrapper}) => {
+    const galleryWrapper = document.querySelector(selectorGalleryWrapper);
+    const photoWrapper = document.querySelector(selectorPhotoWrapper);
+    
+    if (galleryWrapper) {
+        const photos = await getData('data.json');
+        renderGallery(galleryWrapper, photos);
+    }
+
+    if (photoWrapper) {
+        const url = new URL(location.href);
+        console.log(url.searchParams.get('photo'));
+        const photo = await getData('photo.json');
+        renderPhoto(photoWrapper, photo);
+    }
 };
-init();
+init({
+    selectorGalleryWrapper: '.gallery__wrapper',
+    selectorPhotoWrapper: '.photo__wrapper', 
+});
